d1b8b869c538aa7a00dc518ae8d1b4ace72c9210
Jakub Rada <jacub.rada@gmail.com>
2022-01-15 23:01:02 +0100

    tests for logistic regression functions

--- test/runtests.jl
+++ test/runtests.jl
@@ -2,5 +2,22 @@ using titanic
 using Test
 
 @testset "titanic.jl" begin
-    # Write your tests here.
+
+    @testset "logistic regression" begin
+        X = [1.0 1.0 1.0; 1.0 2.0 3.0]
+        k = [1, -1, -1]
+        w = [1.5, -0.5]
+
+        @test isapprox(titanic.computeE(X, k, w), 0.66016; atol=1e-5)
+
+        @test all(isapprox.(titanic.gradE(X, k, w), [0.28450, 0.82532]; atol=1e-5))
+
+        @testset "||$(x)|| = √2" for x in [[1, 1], [-1, -1], [-1, 1], [1, -1]]
+            @test isapprox(titanic.l2norm(x), sqrt(2); atol=1e-5) 
+        end
+
+        @test all(titanic.classify(X, [2.0, -1.0]) .== [1, 1, -1])
+
+    end
+
 end
